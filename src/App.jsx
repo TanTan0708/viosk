1c1edfae31e944673ee317f92439f5cddb6d2c55
Ethan Jeff Mernilo <ethanjeffmernilo@gmail.com>
2026-02-08 22:32:54 +0800

    Bug Fixing on Shopping Cart Feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import Header from "./Header";
 import ProductModal from "./ProductModal";
-import ShoppingCart from "./ShoppingCart";
+import ShoppingCart from "./Shoppingcart";
 
 function App() {
     const [products, setProducts] = useState([]);
